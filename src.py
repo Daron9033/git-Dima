71d11e5a96a932d2781132436b13eace9f748384
Daron <Daron9033@gmail.com>
2022-08-23 12:35:38 +0300

    fff

--- src.py
+++ src.py
@@ -1,2 +1,20 @@
 print "Dima krasava"
-print "YNWA"
+print "YNWA" \
+      "dsafg" \
+      "asdgdsa" \
+      "sadgadsg" \
+      "asdg" \
+      "ds" \
+      "sadgds" \
+      ":argdg" \
+      "dg" \
+      ":keywordgd" \
+      "gd" \
+      "gd" \
+      "gd" \
+      "gd" \
+      "gd" \
+      "gd" \
+      "gd" \
+      ":argd" \
+      "dg"
